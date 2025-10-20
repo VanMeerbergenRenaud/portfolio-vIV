638fd4c9adaff71d6cbc3ec47eba26edd99ba940
Renaud Vmb <renaud.vanmeerbergen@gmail.com>
2025-10-21 00:51:26 +0200

    Fix: Move Vite to production dependencies and add SEO meta tags

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -27,6 +27,6 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-@vite(['resources/css/app.css', 'resources/js/app.js'])
-@livewireStyles
 @fluxAppearance
+@livewireStyles
+@vite(['resources/css/app.css', 'resources/js/app.js'])
