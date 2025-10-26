a3219bc9cf2b49282f9936aca939213a43cc1c54
Renaud Vmb <renaud.vanmeerbergen@gmail.com>
2025-10-27 00:07:35 +0100

    Create a custom background (small detail)

---
+++ resources/views/components/custom-bg.blade.php
@@ -0,0 +1,18 @@
+{{-- Background animé --}}
+<div class="fixed inset-0 pointer-events-none select-none z-0 overflow-hidden"
+     x-data="{
+            translateX: -5,
+            translateY: 5,
+            init() {
+                setInterval(() => {
+                    this.translateX = -15 + Math.random() * 30;
+                    this.translateY = -35 + Math.random() * 70;
+                }, 500);
+            }
+         }">
+    <div class="absolute w-full h-full">
+        <div class="absolute opacity-[0.05] -inset-[200%] w-[500%] h-[500%] transition-transform duration-500 ease-in-out"
+             :style="`background-image: url('{{ asset('img/bg.png') }}'); will-change: transform; transform: translateX(${translateX}%) translateY(${translateY}%)`">
+        </div>
+    </div>
+</div>
