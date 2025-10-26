0fe78c2e11b11e68f94be42de45b9d3e5bfdbe2f
Renaud Vmb <renaud.vanmeerbergen@gmail.com>
2025-10-27 00:06:58 +0100

    Remove partials and enhance the web router

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,29 @@
 <?php
 
+use App\Livewire\Pages\About;
+use App\Livewire\Pages\Contact;
+use App\Livewire\Pages\Home;
+use App\Livewire\Pages\Projects\Index;
+use App\Livewire\Pages\Projects\Show;
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::middleware(['guest'])->group(function () {
+    Route::get('/', Home::class)->name('home');
+    Route::get('/about', About::class)->name('about');
+    Route::get('/projects', Index::class)->name('projects');
+    Route::get('/project/{id}', Show::class)->name('project');
+    Route::get('/contact', Contact::class)->name('contact');
+});
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-Route::middleware(['guest'])->group(function () {
-    Route::view('/about', 'livewire.pages.about')->name('about');
-    Route::view('/projects', 'livewire.pages.projects.index')->name('projects');
-    Route::view('/project/{id}', 'livewire.pages.projects.show')->name('project');
-    Route::view('/contact', 'livewire.pages.contact')->name('contact');
-});
-
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
-
     Route::get('settings/profile', Profile::class)->name('settings.profile');
     Route::get('settings/password', Password::class)->name('settings.password');
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
