962130745c7ee759d94c641842ed720793d7c7a9
Renaud Vmb <renaud.vanmeerbergen@gmail.com>
2025-10-27 16:47:41 +0100

    Add a blog page

---
+++ app/Livewire/Pages/Blog.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire\Pages;
+
+use Livewire\Component;
+
+class Blog extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.blog')
+            ->layout('layouts.guest');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Livewire\Pages\About;
+use App\Livewire\Pages\Blog;
 use App\Livewire\Pages\Contact;
 use App\Livewire\Pages\Home;
 use App\Livewire\Pages\Projects\Index;
@@ -15,6 +16,7 @@ Route::middleware(['guest'])->group(function () {
     Route::get('/about', About::class)->name('about');
     Route::get('/projects', Index::class)->name('projects');
     Route::get('/project/{id}', Show::class)->name('project');
+    Route::get('/blog', Blog::class)->name('blog');
     Route::get('/contact', Contact::class)->name('contact');
 });
 
