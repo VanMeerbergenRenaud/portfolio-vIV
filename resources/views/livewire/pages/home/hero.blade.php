0c2369da2867bf350c738689bbc93be3b526e5b7
Renaud Vmb <renaud.vanmeerbergen@gmail.com>
2025-11-27 14:21:14 +0100

    Update hero image alt text and add GOAT label

--- resources/views/livewire/pages/home/hero.blade.php
+++ resources/views/livewire/pages/home/hero.blade.php
@@ -19,9 +19,12 @@
                     @if($hero->hero_image)
                         <img
                             src="{{ Storage::disk('s3')->url($hero->hero_image) }}"
-                            alt="{{ $hero->hero_image_alt ?? 'Portait de Renaud entouré de chèvre pour montrer qu\'il est le "Goat" (Greatest Of All Time) des développeurs' }}"
+                            alt="{{ $hero->hero_image_alt ?? 'Portrait de Renaud Van Meerbergen' }}"
                             class="w-full h-full min-h-50 max-h-250 object-cover transition-transform duration-500 group-hover:scale-105"
                         >
+                        <span class="absolute bottom-0 right-0 md:bottom-2 md:right-3 text-white text-xs md:text-sm font-semibold py-2 px-4 backdrop-blur-[5px] rounded-lg">
+                            Le "GOAT" des devs
+                        </span>
                     @else
                         <img
                             src="{{ asset('img/placeholder.png') }}"
